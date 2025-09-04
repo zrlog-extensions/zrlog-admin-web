1994ad9f8ffa75dec91f422a80710be824ee40d1
xiaochun <90xchun@gmail.com>
2025-09-04 22:19:40 +0800

    Fix response error

--- src/main/java/com/zrlog/admin/web/plugin/FaasUpdateVersionHandler.java
+++ src/main/java/com/zrlog/admin/web/plugin/FaasUpdateVersionHandler.java
@@ -19,20 +19,21 @@ public class FaasUpdateVersionHandler implements UpdateVersionHandler {
         String finalName = "zrlog-" + NativeUtils.getRealFileArch() + "-latest.zip";
         String lambdaRepoName = Objects.requireNonNullElse(System.getenv("LAMBDA_S3_REPO_NAME"), "zrlog-update-bucket");
         String s3FileUrl = "s3://" + lambdaRepoName + "/" + finalName;
-        return "UA=\"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36\"\n" +
+        return "cd /tmp\n" +
+                "UA=\"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36\"\n" +
                 "wget --user-agent=\"${UA}\" -O \"" + finalName + "\" \"" + downloadUrl + "\"\n" +
                 "aws s3 cp " + finalName + " " + s3FileUrl + "\n" +
                 "aws lambda update-function-code \\\n" +
                 "  --function-name " + functionName + " \\\n" +
                 "  --s3-bucket " + lambdaRepoName + " \\\n" +
-                "  --s3-key " + finalName + "\n\n\n";
+                "  --s3-key " + finalName + "\n\n";
     }
 
     @Override
     public String getMessage() {
         String downloadUrl = version.getZipDownloadUrl().replaceFirst(".zip", "-faas.zip");
         if (EnvKit.isLambda()) {
-            return "#### Update by aws-cli (CloudShell) \n```bash\n" +
+            return "#### Update by aws-cli or CloudShell \n```bash\n" +
                     getS3UpdateShell(downloadUrl, System.getenv("AWS_LAMBDA_FUNCTION_NAME")) +
                     "```";
         }
