296a071895fd2d9f611e25a8af3b25ddd66b7412
xiaochun <90xchun@gmail.com>
2025-11-12 22:53:38 +0800

    Optimize paste upload

--- src/main/frontend/src/common/editor/paste-upload.tsx
+++ src/main/frontend/src/common/editor/paste-upload.tsx
@@ -19,48 +19,70 @@ const PasteUpload: FunctionComponent<PasteUploadProps> = ({
 }) => {
     const axiosInstance = useAxiosBaseInstance(getContainer);
 
-    function uploadFile(file: File | null) {
-        const index = Math.random().toString(10).substr(2, 5) + "-" + Math.random().toString(36).substr(2);
-
-        const fileName = index + ".png";
-        const formData = new FormData();
-        if (file) {
-            formData.append("imgFile", file, fileName);
-            if (onUploading) {
-                onUploading();
-            }
-            axiosInstance
-                .post("/api/admin/upload?dir=image", formData)
-                .then(({ data }) => {
-                    const url = data.data.url;
-                    if (isStaticPage() && url.startsWith("/")) {
-                        onUploadSuccess(getBackendServerUrl() + data.data.url.substring(1));
-                        return;
-                    }
-                    onUploadSuccess(url);
-                })
-                .catch(() => {
-                    if (onUploadFailure) {
-                        onUploadFailure();
-                    }
-                });
+    const getFileExt = (filename: string, defaultExt: string) => {
+        if (!filename.includes(".")) {
+            return defaultExt;
         }
-    }
+        let ext = filename.substring(filename.lastIndexOf(".") + 1);
+        if (ext.trim().length === 0) {
+            ext = defaultExt;
+        }
+        return ext;
+    };
 
-    const doUpload = (e: ClipboardEvent) => {
+    const uploadFile = async (file: File): Promise<string> => {
+        const index = Math.random().toString(10).substr(2, 5) + "-" + Math.random().toString(36).substr(2);
+        const fileName = index + "." + getFileExt(file.name, "jpg");
+        const formData = new FormData();
+        formData.append("imgFile", file, fileName);
+        try {
+            const { data } = await axiosInstance.post("/api/admin/upload?dir=image", formData);
+            const url = data.data.url;
+            if (isStaticPage() && url.startsWith("/")) {
+                return getBackendServerUrl() + data.data.url.substring(1);
+            }
+            return url;
+        } catch (e) {
+            return "";
+        }
+    };
+
+    const doUpload = async (e: ClipboardEvent) => {
         const clipboardData = e.clipboardData;
         if (clipboardData === null) {
             return;
         }
         const items = clipboardData.items;
+        const imgFiles: File[] = [];
         for (let i = 0; i < items.length; i++) {
             if (items[i].kind === "file" && items[i].type.match(/^image/)) {
-                // 取消默认的粘贴操作
-                e.preventDefault();
-                // 上传文件
-                uploadFile(items[i].getAsFile());
-                break;
+                const file = items[i].getAsFile();
+                if (file) {
+                    imgFiles.push(file);
+                }
             }
+        }
+        if (imgFiles.length > 0) {
+            // 取消默认的粘贴操作
+            e.preventDefault();
+            if (onUploading) {
+                onUploading();
+            }
+            const data = await Promise.all(
+                imgFiles.map(async (e) => {
+                    // 上传文件
+                    return await uploadFile(e);
+                })
+            );
+            data.forEach((e) => {
+                if (e.length === 0) {
+                    if (onUploadFailure) {
+                        onUploadFailure();
+                    }
+                } else {
+                    onUploadSuccess(e);
+                }
+            });
         }
     };
 
