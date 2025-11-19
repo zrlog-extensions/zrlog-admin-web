9f104b7e3cad7fc9a68e7dc05071f5d9a1cf8550
xiaochun <90xchun@gmail.com>
2025-11-19 20:32:08 +0800

    Optimize reload page

--- src/main/frontend/src/components/StaticSite.tsx
+++ src/main/frontend/src/components/StaticSite.tsx
@@ -53,7 +53,9 @@ const StaticSite: FunctionComponent<AdminCommonProps<StaticSiteData>> = ({ data 
                                 synced: data.data.synced,
                                 syncing: false,
                             });
-                            window.location.reload();
+                            const url = new URL(window.location.href);
+                            url.searchParams.set("_t", new Date().getTime() + "");
+                            window.location.replace(url.toString());
                         } else {
                             messageApi.info("同步未完成");
                             setState({
